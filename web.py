d5fa0af95d4d795fd7f918e44a959e7998497c3d
wesleykoe <wesleykoe123@gmail.com>
2024-07-25 02:01:34 +0700

    Added some notes to web.py on how to add pages and adjust text fonts

--- web.py
+++ web.py
@@ -5,16 +5,19 @@ import function as fun
 
 todos = fun.get_todos()
 
+st.set_page_config(layout="wide")  # makes your app wider
 
 def add_todo():
     todo = st.session_state["new_todo"] + "\n" # session_state object type
     todos.append(todo)
     fun.write_todos(todos)
 
+# to add a page to your app, create a new directory ("pages) with other .py files
+
 
 st.title("My Todo App")
 st.subheader("This is my todo app")
-st.write("This app will increase your productivity")
+st.write("This app will increase your <b>productivity.")  # unsafe_allow_html=True is to enable fonts e.g. "<b><h1>
 
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)  # key=todo checks which object is being picked
